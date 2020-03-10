3573f1ad76bf19d9c8a7b83eee69130a5b473391
s18371 <s18371@pjwstk.edu.pl>
2020-03-10 12:42:22 +0100

    obiekty

---
+++ Cwiczenia_2/Cwiczenia_2/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+[Serializable]
+public class Student
+{
+    [XmlElement(ElementName = "InneNazwa")]
+    public string Imie { get; set; }
+    [XmlAttribute(AttributeName = "InnaNazwa")]
+    [JsonPropertyName("LastName")]
+    public string Nazwisko { get; set; }
+    [XmlAttribute(AttributeName = "InnaNazwa")]
+    public string Email { get; set; }
+
+    public string Eska { get; set; }
+    public string Kireunek { get; set; }
+
+    public string TrybSt { get; set; }
+
+    public string DataUr { get; set; }
+    public string ImieM { get; set; }
+    public string ImieO { get; set; }
+
+}
---
+++ Cwiczenia_2/Cwiczenia_2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Cwiczenia_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Dane\dane.csv";
+            Console.WriteLine("Hello World");
+
+            //wczytywanie
+            var fi = new FileInfo(path);
+            var list = new List<Student>();
+            using (var stream = new StreamReader(fi.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    string[] student = line.Split(',');
+                    var stu = new Student
+                    {
+                        Imie = student[0],
+                        Nazwisko = student[1],
+                        Kireunek = student[2],
+                        Eska = student[3],
+                        DataUr=student[4],
+                        Email = student[5],
+                        ImieM=student[6],
+                        ImieO=student[7]
+                    };
+                    ///foreach Student
+                    ///if
+                    ///list.Add(stu);
+
+                }
+            }
+            //stream.Dispose();
+
+            //xml
+            
+            /*var st = new Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "kowalski@wp.pl",
+            };*/
+            //list.Add(st);
+            /*FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+            serializer.Serialize(writer, list);
+
+            var jsonString = JsonSerializer.Serialize(list);
+            File.WriteAllText("data.json", jsonString);
+            */
+        }
+    }
+}
